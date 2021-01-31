7cb3ce038203cf74ee503cfa1fadc020135480f6
Adriano Amalfi <adrianoamalfi@gmail.com>
2021-01-31 17:10:45 +0000

    fix

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,33 @@ FROM $BUILD_FROM
 
 ENV LANG C.UTF-8
 
-WORKDIR /
+ENV DJANGO_DB_NAME=default 
+ENV DJANGO_SU_NAME=admin 
+ENV DJANGO_SU_EMAIL=admin@example.com
+ENV DJANGO_SU_PASSWORD=changeme 
+ENV DJANGO_SETTINGS_MODULE=wooeyp.settings 
+
+# Install requirements for add-on
+RUN apk add --no-cache python3 py3-pip
+
+# make some useful symlinks that are expected to exist
+RUN cd /usr/bin \
+	&& ln -s idle3 idle \
+	&& ln -s pydoc3 pydoc \
+	&& ln -s python3 python \
+	&& ln -s python3-config python-config
+
+
+# Python 3 HTTP Server serves the current working dir
+# So let's set it to our add-on persistent data directory.
+WORKDIR /data
+
+COPY requirements.txt /
+RUN pip install -r /requirements.txt
+COPY Procfile /app/Procfile
 COPY start.sh /app/start.sh
+
 ENTRYPOINT ["/app/start.sh"]
+CMD ["start"]
 
 LABEL io.hass.version="VERSION" io.hass.type="addon" io.hass.arch="armhf|aarch64|i386|amd64"
--- start.sh
+++ start.sh
@@ -1,1 +1,36 @@
-echo Hello, world!
+#!/bin/bash
+set -e
+
+export DIR="/data/wooeyp/wooeyp"
+export DJANGO_DB_NAME="default" 
+export DJANGO_SU_NAME="admin" 
+export DJANGO_SU_EMAIL="admin@example.com"
+export DJANGO_SU_PASSWORD="changeme" 
+export DJANGO_SETTINGS_MODULE="wooeyp.settings"
+
+if [ "$1" = 'start' ]; then 
+    if [ -d "$DIR" ]; then
+        ### Take action if $DIR exists ###
+        echo "Folder ${DIR} exist..."
+        echo "Run python manage scripts..."
+        cd wooeyp
+        python manage.py makemigrations
+        python manage.py migrate                  # Apply database migrations
+        python manage.py collectstatic --noinput  # Collect static files
+        [ -f /app/Procfile ] && mv /app/Procfile .
+    else
+        echo "Folder ${DIR} not exist..."
+        echo "Installing files in ${DIR}..."
+        wooify -p wooeyp 
+        cd wooeyp
+        python manage.py makemigrations
+        python manage.py migrate                  # Apply database migrations
+        python manage.py collectstatic --noinput  # Collect static files       
+        echo "Now creating Superuser (admin:chageme)..."
+        python -c "import django; django.setup(); from django.contrib.auth.management.commands.createsuperuser import get_user_model; get_user_model()._default_manager.db_manager('$DJANGO_DB_NAME').create_superuser( username='$DJANGO_SU_NAME', email='$DJANGO_SU_EMAIL', password='$DJANGO_SU_PASSWORD')" # buildkit 418kB buildkit.dockerfile.v0
+        [ -f /app/Procfile ] && mv /app/Procfile .
+    fi
+    exec honcho "$@"
+fi
+
+exec "$@"
